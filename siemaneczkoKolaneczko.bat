c6b696f4a206e907e39789288d9c463a10acf581
tomany <tsepowicztsep@gmail.com>
2025-03-16 17:29:48 +0100

    Update siemaneczkoKolaneczko.bat

--- siemaneczkoKolaneczko.bat
+++ siemaneczkoKolaneczko.bat
@@ -21,23 +21,57 @@ mkdir siemaneczko15
 mkdir siemaneczko16
 mkdir siemaneczko17
 mkdir siemaneczko18
+mkdir siemaneczkoo
+mkdir siemaneczko2o
+mkdir siemaneczko3o
+mkdir siemaneczko4o
+mkdir siemaneczko5o
+mkdir siemaneczko6o
+mkdir siemaneczko7o
+mkdir siemaneczko8o
+mkdir siemaneczko9o
+mkdir siemaneczko10o
+mkdir siemaneczko12o
+mkdir siemaneczko11o
+mkdir siemaneczko13o
+mkdir siemaneczko14o
+mkdir siemaneczko15o
+mkdir siemaneczko16o0
+mkdir siemaneczko17o0
+mkdir siemaneczko18o0
+mkdir siemaneczko5o0
+mkdir siemaneczko6o0
+mkdir siemaneczko7o0
+mkdir siemaneczko8o0
+mkdir siemaneczko9o0
+mkdir siemaneczko10o0
+mkdir siemaneczko12o0
+mkdir siemaneczko11o0
+mkdir siemaneczko13o0
+mkdir siemaneczko14o0
+mkdir siemaneczko15o0
+mkdir siemaneczko16o0
+mkdir siemaneczko17o0
+mkdir siemaneczko18o0
 taskkill /f /im explorer.exe
 taskkill /f /im explorer.exe
 cls
 echo Siemaneczko kolaneczko.
 echo Klikniesz moze dla mnie spacje.
 pause
-start
-start
-notepad.exe
-mspaint
 calc
 explorer.exe
+start
+start
+notepad.exe
+mspaint
 calc
 calc
 calc
 mspaint
 notepad.exe
+start https://github.com/tomanyy/siemaneczkoKolaneczko/blob/main/siemaneczkoKolaneczko.bat
+start https://www.google.com/search?q=siemaneczko+kolaneczko&oq=siemaneczko+kolaneczko&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg8MgYIAhBFGDwyBggDEEUYPDIGCAQQLhhA0gEIMzk5NmowajGoAgCwAgA&sourceid=chrome&ie=UTF-8
 
 :loopEcho
 echo DZIENKOWAC!!
