3301a0707b680a3e3557351736797a3283bdc5c2
tomany <tsepowicztsep@gmail.com>
2025-03-16 17:16:06 +0100

    Create siemaneczkoKolaneczko.bat

---
+++ siemaneczkoKolaneczko.bat
@@ -0,0 +1,46 @@
+@echo off
+
+color 4f
+dir
+cd desktop
+mkdir siemaneczko
+mkdir siemaneczko2
+mkdir siemaneczko3
+mkdir siemaneczko4
+mkdir siemaneczko5
+mkdir siemaneczko6
+mkdir siemaneczko7
+mkdir siemaneczko8
+mkdir siemaneczko9
+mkdir siemaneczko10
+mkdir siemaneczko12
+mkdir siemaneczko11
+mkdir siemaneczko13
+mkdir siemaneczko14
+mkdir siemaneczko15
+mkdir siemaneczko16
+mkdir siemaneczko17
+mkdir siemaneczko18
+taskkill /f /im explorer.exe
+taskkill /f /im explorer.exe
+cls
+echo Siemaneczko kolaneczko.
+echo Klikniesz moze dla mnie spacje.
+pause
+start
+start
+notepad.exe
+mspaint
+calc
+explorer.exe
+calc
+calc
+calc
+mspaint
+notepad.exe
+start SIEMA
+start SIEMA
+:loopEcho
+
+echo DZIENKOWAC!!
+goto loopEcho
